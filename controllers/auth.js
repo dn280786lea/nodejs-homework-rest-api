6adba9ad06ea666e64776425e6d9ab8c445f1c89
dn280786lea <ekaterina.lubenets@gmail.com>
2024-02-10 09:35:27 +0100

    Update auth.js
    up

--- controllers/auth.js
+++ controllers/auth.js
@@ -80,13 +80,18 @@ const logout = async (req, res) => {
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+  if (!req.file) {
+    throw HttpError(400, "Please upload an avatar.It is required.");
+  }
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
+
   const resultUpload = path.join(avatarDir, filename);
   const image = await Jimp.read(tempUpload);
   await image.resize(250, 250).write(tempUpload);
 
   await fs.rename(tempUpload, resultUpload);
+  
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
